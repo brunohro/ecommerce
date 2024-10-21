29cfe2e17d7e7e1a7fd60168692bda29efd6c619
ketilyk21 <ketily.k@escolar.ifrn.edu.br>
2024-10-21 08:06:41 -0300

    Add models

--- loja/admin.py
+++ loja/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Categoria, Produto
 # Register your models here.
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome')
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome')
+
+admin.site.register(Categoria)
+admin.site.register(Produto)
