3ba2606b49fdad6246c559e8588475206255775f
ItaloGui98 <italoguilherme974@gmail.com>
2024-11-12 19:13:03 -0300

    testando novo layout

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Produto, Categoria, CarrinhoItem, Cliente
 from ecommerce.forms import ClienteForm
 
