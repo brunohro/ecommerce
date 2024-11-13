9ed3f8156b509f2332b27dd2cf6b1999bcf01a58
Bruno Henrique Oliveira <bh084996074995@gmail.com>
2024-11-13 12:38:02 -0300

    Finalizando CRUD de cliente

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -7,21 +7,43 @@ def index(request):
     categoria = Categoria.objects.all()
     return render(request, 'index.html', {'produtos': produto, 'categorias': categoria})
 
-def ofertas(request):
-    produto = Produto.objects.all()
-    categoria = Categoria.objects.all()
-    return render(request, 'ofertas.html', {'produtos': produto, 'categorias': categoria})
-
 def cadastrar_cliente(request):
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
-    else: 
+            return redirect('index')  # Redireciona para a página inicial ou qualquer página desejada
+    else:
         form = ClienteForm()
     return render(request, 'clientes/cadastrar_cliente.html', {'form': form})
 
+
+def editar_cliente(request, id):
+    cliente = get_object_or_404(Cliente, id=id)
+    cliente = Cliente.objects.get(id=id)
+    if request.method == 'POST':
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save() 
+            return redirect('index')
+    else:
+        form = ClienteForm(instance=cliente)
+    return render(request, 'clientes/cadastrar_cliente.html', {'form': form})
+
+
+def remover_cliente(request, id):
+    cliente = get_object_or_404(Cliente, id=id)
+    cliente = Cliente.objects.get(id=id)
+    cliente.delete() 
+    return redirect('administrador')
+    #return render(request, 'clientes/remover_cliente.html', {'cliente': cliente})  # Exibe confirmação para remover
+
+
+
+def ofertas(request):
+    produto = Produto.objects.all()
+    categoria = Categoria.objects.all()
+    return render(request, 'ofertas.html', {'produtos': produto, 'categorias': categoria})
 def carrinho(request):
     return render(request, 'loja/carrinho.html')
 
