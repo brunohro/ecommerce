a8feb7a37589453596ef66f33914c0d96940edf9
ItaloGui98 <italoguilherme974@gmail.com>
2024-11-13 14:22:26 -0300

    mudando código do script.js

--- static/js/script.js
+++ static/js/script.js
@@ -12,30 +12,3 @@ form.addEventListener('submit', function(event) {
 });
 
 // ------------------------------------------------------------------------------------------------------------------
-
-// Função para atualizar o preço total de um item
-function atualizarPrecoTotal(id) {
-    // Seleciona o campo de quantidade e o preço do produto
-    const quantidadeInput = document.querySelector(`input[data-id="${id}"]`);
-    const precoUnitario = parseFloat(quantidadeInput.getAttribute('data-preco'));
-    const quantidade = parseInt(quantidadeInput.value);
-
-    // Calcula o novo preço total
-    const precoTotal = precoUnitario * quantidade;
-
-    // Atualiza o campo de preço total na página
-    const precoTotalElement = document.getElementById(`preco-total-${id}`);
-    precoTotalElement.textContent = `Preço total: R$ ${precoTotal.toFixed(2)}`;
-}
-
-console.log("OIIIIII")
-
-// Adiciona evento 'input' para atualizar o preço total ao mudar a quantidade
-document.querySelectorAll('.quantidade').forEach(input => {
-    console.log("Elemento detectado:", input);
-    input.addEventListener('input', (event) => {
-        const id = event.target.getAttribute('data-id');
-        console.log(id)
-        atualizarPrecoTotal(id);
-    });
-});
