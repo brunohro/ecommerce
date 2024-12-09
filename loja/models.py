dc1cd577b4bc0811cbecdd8b3015ce99e593ea91
ItaloGui98 <italoguilherme974@gmail.com>
2024-12-09 17:21:55 -0300

    corrigindo problema ao criar um super usuário

--- loja/models.py
+++ loja/models.py
@@ -84,7 +84,7 @@ class Cliente(AbstractUser):
     ativo = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username', 'cpf']
 
     objects = CustomManager()
 
